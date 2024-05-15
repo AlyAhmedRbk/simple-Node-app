c5f59ac4bff5bb675a19b28f1fadb21744667011
Aly Ahmed <113694375+AlyAhmedRbk@users.noreply.github.com>
2024-05-15 09:32:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 console.log("Hello Jenkins!");
 
-const add = (a, b) =>{
-  return a+b
-}
 
-console.log("Sum of the value = ",add(5,5));
-console.log("Hello Nodejs from github.");
+console.log("Hello Jenkins from cloud");
